f7b6681e90b87c5368f78cccdc86989fd7e431b7
nguyenduongtuan <125101917+nguyenduongtun@users.noreply.github.com>
2024-08-05 00:48:15 +0700

    Update BTVN_Bai9.cpp

--- BTVN1/BTVN_Bai9.cpp
+++ BTVN1/BTVN_Bai9.cpp
@@ -7,26 +7,26 @@ void main()
 	int tong, hieu, tich;
 	float thuong;
 
-	// G·n gi· tr? cho a v‡ b
+	
 	printf("Nhap so a: ");
 	scanf_s("%d", &a);
 	printf("Nhap so b: ");
 	scanf_s("%d", &b);
 
-	// G·n ??a ch? c?a a v‡ b cho c·c con tr? pa v‡ pb
+	
 	pa = &a;
 	pb = &b;
 
-	// TÌnh to·n t?ng, hi?u, tÌch, v‡ th??ng
+	
 	tong = *pa + *pb;
 	hieu = *pa - *pb;
 	tich = (*pa) * (*pb);
-	thuong = (float)(*pa) / (*pb); // …p ki?u ?? tÌnh th??ng chÌnh x·c h?n
+	thuong = (float)(*pa) / (*pb); 
 
 	
-	printf("Tong cua %d v‡ %d l‡: %d, tai dia chi: %p\n", *pa, *pb, tong, (void*)&tong);
-	printf("Hieu cua %d v‡ %d l‡: %d, tai dia chi: %p\n", *pa, *pb, hieu, (void*)&hieu);
-	printf("Tich cua %d v‡ %d l‡: %d, tai dia chi: %p\n", *pa, *pb, tich, (void*)&tich);
-	printf("Thuong cua %d v‡ %d l‡: %.2f, tai dia chi: %p\n", *pa, *pb, thuong, (void*)&thuong);
+	printf("Tong cua %d v√† %d l√†: %d, tai dia chi: %p\n", *pa, *pb, tong, (void*)&tong);
+	printf("Hieu cua %d v√† %d l√†: %d, tai dia chi: %p\n", *pa, *pb, hieu, (void*)&hieu);
+	printf("Tich cua %d v√† %d l√†: %d, tai dia chi: %p\n", *pa, *pb, tich, (void*)&tich);
+	printf("Thuong cua %d v√† %d l√†: %.2f, tai dia chi: %p\n", *pa, *pb, thuong, (void*)&thuong);
 
 }
