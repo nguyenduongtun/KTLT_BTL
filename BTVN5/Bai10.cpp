a3436a6718a98b6c00ac795bccd1a1812f346f22
nguyenduongtuan <125101917+nguyenduongtun@users.noreply.github.com>
2024-08-15 00:02:16 +0700

    Add files via upload
    Up bai 9 10 trong BTVN5

---
+++ BTVN5/Bai10.cpp
@@ -0,0 +1,54 @@
+﻿#include<stdio.h>
+
+long long Fibonacci(int n)
+{
+    if (n <= 2)
+    {
+        return 1;
+    }
+    else
+    {
+        long long a = 1, b = 1, c = 0;
+        for (int i = 3; i <= n; i++)
+        {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return c;
+    }
+}
+
+// Hàm in các số Fibonacci thuộc đoạn [m, n]
+void Xuat_Fibonacci(int m, int n)
+{
+    int i = 1;
+    long long fibo;
+    printf("Cac so Fibonacci thuoc doan [%d, %d]:\n", m, n);
+    while (1)
+    {
+        fibo = Fibonacci(i);
+        if (fibo > n)
+        {
+            break;
+        }
+        if (fibo >= m)
+        {
+            printf("%lld ", fibo);
+        }
+        i++;
+    }
+    printf("\n");
+}
+
+int main()
+{
+    int m, n;
+    printf("Nhap gia tri m: ");
+    scanf_s("%d", &m);
+    printf("Nhap gia tri n: ");
+    scanf_s("%d", &n);
+    Xuat_Fibonacci(m, n);
+
+    return 0;
+}
---
+++ BTVN5/Bai9.cpp
@@ -0,0 +1,26 @@
+//#include <stdio.h>
+//
+//// khu de quy
+//long long CapSoNhankhudequy(int n, long long a, long long q) {
+//	long long result = a;
+//	for (int i = 1; i < n; i++) 
+//	{
+//		result *= q;
+//	}
+//	return result;
+//}
+//
+//int main() 
+//{
+//	int n;
+//	long long a, q;
+//	printf("Nhap gia tri hang dau a: ");
+//	scanf_s("%lld", &a);
+//	printf("Nhap gia tri cong boi q: ");
+//	scanf_s("%lld", &q);
+//	printf("Nhap gia tri phan tu thu n: ");
+//	scanf_s("%d", &n);
+//	long long result = CapSoNhankhudequy(n, a, q);
+//	printf("Phan tu thu %d cua cap so nhan la: %lld\n", n, result);
+//	return 0;
+//}
